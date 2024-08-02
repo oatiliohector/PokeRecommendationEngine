b987a961a9051176f8d91a5bae4bc87f78241e8c
Atilio <atilio.jesus@grupoboticario.com.br>
2024-08-01 22:03:51 -0300

    feat: ✨ first structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div >
+      <h1>Game Recommendation</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+// 1. Game Recommendation W
